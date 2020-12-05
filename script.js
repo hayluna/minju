3b7bf4c2a1bd29998e732fdba238557fdd8efd02
HaeEun Myung <hayluna2@gmail.com>
2020-12-05 19:19:55 +0900

    헤더, 풋터 분리, 스크립트로 url파싱해서 컨텐츠 불러오기

---
+++ script.js
@@ -0,0 +1,9 @@
+var getQueryParams = function (location) {
+  var queryString = location.search.substring(1);
+  var qs = queryString.split('&').map(function (q) {
+    return q.split('=')[1];
+  });
+  var chapter = qs[0];
+  var article = qs[1];
+  return { chapter, article };
+};
