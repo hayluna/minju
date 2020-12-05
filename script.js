751f72748ef4501e8ae249455822cccfecfa8502
Hae Eun Myung <hayluna2@gmail.com>
2020-12-06 00:35:50 +0900

    메뉴와 모바일 작업

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var getQueryParams = function (location) {
   var queryString = location.search.substring(1);
-  var qs = queryString.split('&').map(function (q) {
-    return q.split('=')[1];
+  var qs = queryString.split("&").map(function (q) {
+    return q.split("=")[1];
   });
   var chapter = qs[0];
   var article = qs[1];
@@ -9,13 +9,5 @@ var getQueryParams = function (location) {
 };
 
 var toggleSlideMenu = function () {
-  if ($('#side-menu').hasClass('open')) {
-    $('#side-menu').removeClass('open');
-    $('#side-menu').css('color', 'transparent');
-    // $('#side-menu').css('width', '0px');
-  } else {
-    $('#side-menu').addClass('open');
-    // $('#side-menu').css('width', '600px');
-    // $('#side-menu').css('width', '0px');
-  }
+  $("#menu").toggleClass("open");
 };
