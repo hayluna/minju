c2e6e86f6a1a6338356001bc8c762b55f1139af6
HaeEun Myung <hayluna2@gmail.com>
2020-12-05 20:43:51 +0900

    메뉴 만드는 중

--- script.js
+++ script.js
@@ -7,3 +7,15 @@ var getQueryParams = function (location) {
   var article = qs[1];
   return { chapter, article };
 };
+
+var toggleSlideMenu = function () {
+  if ($('#side-menu').hasClass('open')) {
+    $('#side-menu').removeClass('open');
+    $('#side-menu').css('color', 'transparent');
+    // $('#side-menu').css('width', '0px');
+  } else {
+    $('#side-menu').addClass('open');
+    // $('#side-menu').css('width', '600px');
+    // $('#side-menu').css('width', '0px');
+  }
+};
