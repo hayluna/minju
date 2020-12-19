11b046924aefdbbe0260cf2cd4b2c5d025821540
Hae Eun Myung <hayluna2@gmail.com>
2020-12-19 11:34:19 +0900

    private page 접속시 index.html로 redirect

--- script.js
+++ script.js
@@ -5,9 +5,23 @@ var getQueryParams = function (location) {
   });
   var chapter = qs[0];
   var article = qs[1];
-  return { chapter:chapter, article:article };
+  return { chapter: chapter, article: article };
 };
 
 var toggleSlideMenu = function () {
   $("#menu").toggleClass("open");
+};
+
+var redirect = function (location) {
+  var queryString = location.search;
+  switch (location.pathname) {
+    case "/header.html":
+    case "/footer.html":
+      window.location.href = "/index.html";
+    case "/detail.html":
+      if (queryString === "") {
+        window.location.href = "/index.html";
+      }
+      break;
+  }
 };
